731bbedb629770d11c5795b2a409d4e3e6714911
Fantasy <jie844067636@gmail.com>
2019-02-22 15:26:26 +0800

    feat: Support observe data deeply

--- utils/base.js
+++ utils/base.js
@@ -1,30 +1,130 @@
 /// <reference path="../@types/index.d.ts" />
 
+/**
+ *
+ * @param {JSON} data
+ */
 function JSONClone(data) {
   return JSON.parse(JSON.stringify(data || {}));
 }
 
 /**
  *
- * @param {BasePage} target
+ * @param {*} obj
+ * @param {string} prop
+ * @param {*} val
+ * @param {boolean} [enumerable ]
  */
-function bindData(target) {
-  const data = target.data;
-
-  Object.keys(data).forEach(key => {
-    Object.defineProperty(data, key, {
-      set(val) {
-        const changedData = {};
-        changedData[key] = val;
-        target.setData(changedData);
-      },
-      get() {
-        return target.target.data[key];
-      },
-      configurable: true,
-      enumerable: true
-    });
+function def(obj, prop, val, enumerable = false) {
+  Object.defineProperty(obj, prop, {
+    value: val,
+    enumerable,
+    writable: true,
+    configurable: true
   });
+}
+
+class Observe {
+  /**
+   *
+   * @param {JSON} data
+   * @param {(data:any)=>void} dataChanged
+   * @param {string} [prePath]
+   */
+  constructor(data, dataChanged, prePath = "") {
+    this.dataChanged = dataChanged;
+    this.prefix = prePath;
+    this.data = {};
+    this.subOb = {};
+    def(data, "__observed__", true);
+
+    Object.keys(data).forEach(key => {
+      const value = data[key];
+      this.attachObserve(key, value);
+
+      Object.defineProperty(data, key, {
+        set: val => {
+          this.updateData(key, val);
+
+          this.attachObserve(key, val);
+        },
+        get: () => {
+          return this.safeGet(key);
+        },
+        configurable: true,
+        enumerable: true
+      });
+    });
+  }
+
+  /**
+   *
+   * @param {any[]} arr
+   * @param {string} key
+   */
+  observeArrayMethods(arr, key) {
+    this.data[key] = arr;
+
+    const methods = [
+      "push",
+      "pop",
+      "shift",
+      "unshift",
+      "splice",
+      "sort",
+      "reverse"
+    ];
+
+    methods.forEach(method => {
+      def(arr, method, (...args) => {
+        const originMethod = Array.prototype[method];
+        originMethod.apply(arr, args);
+
+        this.updateData(key, arr);
+      });
+    });
+  }
+
+  /**
+   *
+   * @param {string} key
+   * @param {any} value
+   */
+  updateData(key, value) {
+    const changedData = {};
+    const path = this.prefix ? this.prefix + "." + key : key;
+    changedData[path] = value;
+
+    this.dataChanged(changedData);
+  }
+
+  /**
+   *
+   * @param {string} key
+   * @param {any} value
+   */
+  attachObserve(key, value) {
+    if (value["__observed__"]) {
+      this.data[key] = value;
+    } else if (Array.isArray(value)) {
+      this.observeArrayMethods(value, key);
+    } else if (typeof value === "object") {
+      const prefix = this.prefix ? this.prefix + "." + key : key;
+
+      this.data[key] = value;
+      this.subOb[key] = new Observe(value, this.dataChanged, prefix);
+    } else {
+      this.data[key] = value;
+    }
+  }
+
+  /**
+   *
+   * @param {string} key
+   */
+  safeGet(key) {
+    return this.data[key];
+  }
 }
 
 /**
@@ -58,7 +158,14 @@ function bindPage(target) {
   }
 
   Page(registerObj);
-  bindData(target);
+
+  def(
+    target.data,
+    "__ob__",
+    new Observe(target.data, arg => {
+      target.target.setData(arg);
+    })
+  );
 }
 
 class BasePage {
