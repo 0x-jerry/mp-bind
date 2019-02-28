c1532a902db84668e556dbc606b2bcfbe9ecb64b
cwxyz007 <jie844067636@gmail.com>
2019-02-28 21:27:56 +0800

    refactor: Refactor code

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,11 +4,9 @@ import { BasePage, bindPage } from '../../core/index';
 export class Index extends BasePage {
   arr = [];
 
-  computed = {
-    arrLength() {
-      return this.arr.length;
-    },
-  };
+  get arrLength() {
+    return this.arr.length;
+  }
 
   onLoad() {
     this.arr.push(1);
