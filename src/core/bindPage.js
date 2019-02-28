67b4eb5d4687c57ffe9dc5f9b7e375def026b9e1
cwxyz007 <jie844067636@gmail.com>
2019-02-28 18:25:42 +0800

    test: Test use getter as computed

--- src/core/bindPage.js
+++ src/core/bindPage.js
@@ -12,8 +12,12 @@ function bindPage(Base) {
   const tpl = new Base();
 
   const registerObj = {
-    data: tpl.$data,
-    ...tpl,
+    data: {},
+    computed: {},
+    watch: {},
+    /**
+     * @param {any[]} args
+     */
     onLoad(...args) {
       logger('Page loaded', this);
       if (BaseConfigs.debug) {
@@ -40,7 +44,64 @@ function bindPage(Base) {
     },
   };
 
-  attachFunctions(tpl, registerObj, ['constructor', 'onLoad']);
+  let proto = tpl;
+
+  while (!proto.isPrototypeOf(Object)) {
+    Object.keys(proto).forEach((key) => {
+      const value = tpl[key];
+      if (typeof value === 'function') {
+        // is watch
+        if (/^\$\$/.test(key)) {
+          registerObj.watch[key.slice(2)] = value;
+        } else {
+          registerObj[key] = value;
+        }
+      } else {
+        registerObj.data[key] = value;
+      }
+    });
+
+    Object.getOwnPropertyNames(proto).filter((key) => {
+      const desc = Object.getOwnPropertyDescriptor(proto, key);
+      if (desc.get) {
+        registerObj.computed[key] = desc.get;
+      }
+    });
+
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  //--------------------------------
+  // const registerObj = {
+  //   data: tpl.$data,
+  //   ...tpl,
+  //   onLoad(...args) {
+  //     logger('Page loaded', this);
+  //     if (BaseConfigs.debug) {
+  //       global.page = this;
+  //     }
+
+  //     // @ts-ignore
+  //     const updateQueue = new UpdateTaskQueue(this);
+  //     def(this, BaseConfigs.keys.updateQueue, updateQueue);
+  //     def(this, BaseConfigs.keys.forceUpdate, () => updateQueue.updateData);
+
+  //     this.$data = JSONClone(this.data);
+  //     new Observer(this.$data, (newData, oldData) => {
+  //       // @ts-ignore
+  //       updateData(this, newData, oldData);
+  //     });
+
+  //     // Trigger computed and calculate dependence
+  //     // @ts-ignore
+  //     triggerComputed(this);
+
+  //     // onload
+  //     tpl.onLoad && tpl.onLoad.call(this, args);
+  //   },
+  // };
+
+  // attachFunctions(tpl, registerObj, ['constructor', 'onLoad']);
 
   logger('Register page', registerObj);
   // Register Page
