689e27c8bfbaa2aa713647a7dc0a3c6ca9d780a3
xligem <x.li.gem@gmail.com>
2020-05-28 19:42:44 +0800

    add config

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,15 +1,17 @@
-export const BaseConfigs = {
+export const BaseConfigs: IBaseConfig = {
   debug: false,
+  platform: "wx",
 };
 
 export enum ProxyKeys {
-  PROXY = '__$PROXY$__',
-  DATA = '__$DATA$__',
-  OB = '__$OB$__'
+  PROXY = "__$PROXY$__",
+  DATA = "__$DATA$__",
+  OB = "__$OB$__",
 }
 
 export interface IBaseConfig {
   debug: boolean;
+  platform: "ali" | "wx";
 }
 
 export function setConfig(config: Partial<IBaseConfig> = {}) {
